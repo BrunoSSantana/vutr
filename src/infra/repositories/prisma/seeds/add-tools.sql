4d3f945a2a4b6517da8b03518496b8dccef4d6c7
BrunoSSantana <brunoosouza15@gmail.com>
2023-05-18 22:26:51 -0300

    feat: add seed to create tools

---
+++ src/infra/repositories/prisma/seeds/add-tools.sql
@@ -0,0 +1,22 @@
+INSERT INTO tools (title, link, description)
+VALUES
+  ('React', 'https://reactjs.org/', 'A JavaScript library for building user interfaces.'),
+  ('Angular', 'https://angular.io/', 'A TypeScript-based open-source framework for building web applications.'),
+  ('Vue.js', 'https://vuejs.org/', 'A progressive JavaScript framework for building user interfaces.'),
+  ('Express.js', 'https://expressjs.com/', 'A fast, unopinionated, and minimalist web framework for Node.js.'),
+  ('Next.js', 'https://nextjs.org/', 'A React framework for server-rendered applications and static websites.'),
+  ('Node.js', 'https://nodejs.org/', 'A JavaScript runtime built on Chromes V8 JavaScript engine.'),
+  ('TypeScript', 'https://www.typescriptlang.org/', 'A typed superset of JavaScript that compiles to plain JavaScript.'),
+  ('Webpack', 'https://webpack.js.org/', 'A static module bundler for JavaScript applications.'),
+  ('Babel', 'https://babeljs.io/', 'A toolchain that is mainly used to convert ECMAScript 2015+ code into a backward-compatible version of JavaScript.'),
+  ('Jest', 'https://jestjs.io/', 'A delightful JavaScript testing framework with a focus on simplicity.'),
+  ('Cypress', 'https://www.cypress.io/', 'Fast, easy and reliable testing for anything that runs in a browser.'),
+  ('Redux', 'https://redux.js.org/', 'A predictable state container for JavaScript apps.'),
+  ('GraphQL', 'https://graphql.org/', 'A query language for your API.'),
+  ('ESLint', 'https://eslint.org/', 'A pluggable and configurable linter tool for identifying and reporting on patterns in JavaScript.'),
+  ('Prettier', 'https://prettier.io/', 'An opinionated code formatter that enforces a consistent code style.'),
+  ('Axios', 'https://axios-http.com/', 'A promise-based HTTP client for the browser and Node.js.'),
+  ('Docker', 'https://www.docker.com/', 'A platform for building, shipping, and running applications in containers.'),
+  ('VS Code', 'https://code.visualstudio.com/', 'A lightweight but powerful source code editor developed by Microsoft.'),
+  ('Postman', 'https://www.postman.com/', 'A collaboration platform for API development and testing.'),
+  ('Git', 'https://git-scm.com/', 'A distributed version control system for tracking changes in source code during software development.');
