804a030fec72d9e382a46d976858344516fee178
BrunoSSantana <brunoosouza15@gmail.com>
2023-05-25 22:02:32 -0300

    feat: inicialize firebase in project

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,10 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.string().default('3000'),
   NODE_ENV: z.enum(['development', 'production']).default('development'),
+  FIREBASE_PROJECT_ID: z.string(),
+  FIREBASE_CLIENT_EMAIL: z.string(),
+  FIREBASE_PRIVATE_KEY: z.string().transform((key) => key.replace(/\\n/gi, "\n")),
+  FIREBASE_DATABASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
