32b2ee8292be3a920bb9f739775c597daae5819d
Bruno Santana <brunoosouza15@gmail.com>
2024-02-24 15:38:22 -0300

    fix: compile with tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/server.ts'],
   splitting: false,
   sourcemap: true,
+  format: 'esm',
   clean: true,
 })
